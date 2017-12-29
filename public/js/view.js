982289ca117693ab4ab2e3da09b60b808a212a41
jtmPC <jon87tyler@gmail.com>
2017-12-29 08:00:45 -0700

    sorting in progress

--- public/js/view.js
+++ public/js/view.js
@@ -6,31 +6,71 @@ $(document).ready(() => {
 
 	// ---------- Event Handlers ---------- //
 	$('#propSelectDropDown').on('change', handlePropSelect);
+
+	// $('.tableHeader').on('click', sortTable());
 	
+	
+	// ---------- Functions ---------- //
 	function handlePropSelect() {
     let selectedPropertyID = $(this).val();
     getReq(selectedPropertyID);
   }
 
-	// ---------- Functions ---------- //
 	function getReq (ID) {
 	  $.get("/api/"+ID, (data) => {
-			console.log('Get req', data);
 	    properties = data;
 	    initializeTable(ID);
 	  });
 	}
 
+	function sortTable(n) {
+		let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+		table = $("#propTable");
+		switching = true;
+		dir = "asc"; 
+
+		while (switching) {
+			switching = false;
+			rows = $("tr");
+			for (i = 1; i < (rows.length - 1); i++) {
+				shouldSwitch = false;
+				x = rows[i].$("td")[n];
+				y = rows[i + 1].$("td")[n];
+				if (dir == "asc") {
+					if (x.innerHTML.parseFloat() > y.innerHTML.parseFloat()) {
+						shouldSwitch= true;
+						break;
+					}
+				} else if (dir == "desc") {
+					if (x.innerHTML.parseFloat() < y.innerHTML.parseFloat()) {
+						shouldSwitch= true;
+						break;
+					}
+				}
+			}
+			if (shouldSwitch) {
+				rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+				switching = true;
+				switchcount ++; 
+			} else {
+				if (switchcount == 0 && dir == "asc") {
+					dir = "desc";
+					switching = true;
+				}
+			}
+		}
+	}
+
 	function initializeTable(ID) {
     $display.empty();
     let tableBegin = 
-			"<table class='table'>"+
+			"<table class='table' id='propTable'>"+
 				"<thead>"+
 					"<tr>"+
-						"<th scope='col'>#</th>"+
-						"<th scope='col'>Expense Account</th>"+
-						"<th scope='col'>Expense Amount</th>"+
-						"<th scope='col'>Period End Date</th>"+
+						"<th scope='col' class='tableHeader' onclick='sortTable(0)' >#</th>"+
+						"<th scope='col' class='tableHeader' onclick='sortTable(1)' >Expense Account</th>"+
+						"<th scope='col' class='tableHeader' onclick='sortTable(2)' >Expense Amount</th>"+
+						"<th scope='col' class='tableHeader' onclick='sortTable(3)' >Period End Date</th>"+
 					"</tr>"+
 				"</thead>"+
 				"<tbody>";
@@ -47,32 +87,8 @@ $(document).ready(() => {
 
 		let finishTable = "</tbody></table>";
 		let totalTable = tableBegin+finishTable;
-		console.log(totalTable);
     $display.prepend(totalTable);
   }
 
-  function showProperty (property, i) {
-  	let $eachPropertyDisplay = $(
-  		[
-				"<tr>",
-					"<th scope='row'>",
-					(i+1),
-					"</th>",
-					"<td>",
-					property.AccountID,
-					"</td>",
-					"<td>",
-					"$"+property.ExpenseAmount+",000.00",
-					"</td>",
-					"<td>",
-					property.PeriodEndData,
-					"</td>",
-				"</tr>"
-  		].join("")
-		);
-
-		return $eachPropertyDisplay;
-		console.log($eachPropertyDisplay);
-  }
 
 });
